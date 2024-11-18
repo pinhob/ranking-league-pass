4267d03d07c23a4f54d4bd691d8ef42ea7bf6875
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2024-11-18 19:09:32 +0000

    feat (Home): add cards with data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import localFont from "next/font/local";
+import fs from 'fs';
+import path from "path";
+import { InferGetStaticPropsType } from "next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -11,12 +15,37 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export default function Home() {
+export async function getStaticProps() {
+  const filepath = path.join(process.cwd(), 'src', 'pages', 'api', 'data.json');
+  const jsonData = fs.readFileSync(filepath, 'utf8');
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      cards: data,
+    }
+  }
+}
+
+export default function Home({ cards }: { cards: InferGetStaticPropsType<typeof getStaticProps> }) {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)]`}
     >
-      <h1>Oi, Bola Presa!</h1>
+      {
+        cards.map((card, index) => (
+          <Card key={index}>
+          <CardHeader>
+            <CardTitle>{card.team}</CardTitle>
+            <CardDescription>
+              <p>{card.rating}</p>
+              <p>{card.stamp}</p>
+              <p>{card.over ? "over" : "under"}</p>
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        ))
+      }
     </div>
   );
 }
