ae7be423f8b7bc4a2e0890b811c9637a99812136
pinhob <20286747+pinhob@users.noreply.github.com>
2024-11-19 18:43:35 -0300

    feat (CustomCard): display franchise name based in the provided slug

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -3,6 +3,7 @@ import { Collapsible, CollapsibleTrigger } from "./ui/collapsible";
 import { useState } from "react";
 import { CollapsibleContent } from "@radix-ui/react-collapsible";
 import { ChevronsUpDown } from "lucide-react"
+import franchiseNames from "@/utils/franchiseNames";
 
 interface Data {
     team: string,
@@ -16,10 +17,12 @@ interface Data {
 export default function CustomCard({ data }: { data: Data }) {
     const [isOpen, setIsOpen] = useState(false);
 
+    const franchise = franchiseNames.find(franchise => franchise.slug === data.team);
+
     return (
         <Card>
             <CardHeader>
-                <CardTitle>{data.team}</CardTitle>
+                <CardTitle>{franchise ? franchise.name : data.team}</CardTitle>
                 <CardDescription>
                     <p>{data.rating}</p>
                     <p>{data.stamp}</p>
@@ -42,3 +45,124 @@ export default function CustomCard({ data }: { data: Data }) {
         </Card>
     )
 }
+//     {
+//         "name": "Denver Nuggets",
+//         "slug": "nuggets"
+//     },
+//     {
+//         "name": "Oklahoma City Thunder",
+//         "slug": "thunder"
+//     },
+//     {
+//         "name": "Minnesota Timberwolves",
+//         "slug": "timberwolves"
+//     },
+//     {
+//         "name": "Utah Jazz",
+//         "slug": "jazz"
+//     },
+//     {
+//         "name": "Portland Trail Blazers",
+//         "slug": "trail-blazers"
+//     },
+//     {
+//         "name": "Detroit Pistons",
+//         "slug": "pistons"
+//     },
+//     {
+//         "name": "Milwaukee Bucks",
+//         "slug": "bucks"
+//     },
+//     {
+//         "name": "Cleveland Cavaliers",
+//         "slug": "cavaliers"
+//     },
+//     {
+//         "name": "Indiana Pacers",
+//         "slug": "pacers"
+//     },
+//     {
+//         "name": "Chicago Bulls",
+//         "slug": "bulls"
+//     },
+//     {
+//         "name": "San Antonio Spurs",
+//         "slug": "spurs"
+//     },
+//     {
+//         "name": "Dallas Mavericks",
+//         "slug": "mavericks"
+//     },
+//     {
+//         "name": "Memphis Grizzlies",
+//         "slug": "grizzlies"
+//     },
+//     {
+//         "name": "New Orleans Pelicans",
+//         "slug": "pelicans"
+//     },
+//     {
+//         "name": "Houston Rockets",
+//         "slug": "rockets"
+//     },
+//     {
+//         "name": "Brooklyn Nets",
+//         "slug": "nets"
+//     },
+//     {
+//         "name": "Boston Celtics",
+//         "slug": "celtics"
+//     },
+//     {
+//         "name": "New York Knicks",
+//         "slug": "knicks"
+//     },
+//     {
+//         "name": "Philadelphia 76ers",
+//         "slug": "75ers"
+//     },
+//     {
+//         "name": "Toronto Raptors",
+//         "slug": "raptors"
+//     },
+//     {
+//         "name": "Los Angeles Clippers",
+//         "slug": "clippers"
+//     },
+//     {
+//         "name": "Phoenix Suns",
+//         "slug": "suns"
+//     },
+//     {
+//         "name": "Sacramento Kings",
+//         "slug": "kings"
+//     },
+//     {
+//         "name": "Los Angeles Lakers",
+//         "slug": "lakers"
+//     },
+//     {
+//         "name": "Golden State Warriors",
+//         "slug": "warriors"
+//     },
+//     {
+//         "name": "Washington Wizards",
+//         "slug": "wizards"
+//     },
+//     {
+//         "name": "Charlotte Hornets",
+//         "slug": "hornets"
+//     },
+//     {
+//         "name": "Atlanta Hawks",
+//         "slug": "hawks"
+//     },
+//     {
+//         "name": "Miami Heat",
+//         "slug": "heat"
+//     },
+//     {
+//         "name": "Orlando Magic",
+//         "slug": "magic"
+//     }
+// ];
